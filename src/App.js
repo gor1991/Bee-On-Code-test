f964f57002308ec4c81b85f58e29930a87dd1ece
Gor Melqonyan <gor0591@mail.ru>
2021-12-11 20:21:17 +0400

    category

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header"
+import AddCategory from "./AddCategory"
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <button className="div4">M</button>
+      <button className="div4 box">ж</button>
+      <AddCategory/>
     </div>
   );
 }
